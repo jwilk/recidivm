jwilk/recidivm

commit 153aaf3f39131c977370e51ae311f59e0cc39d1f

Enable LFS.

Makefile
@@ -1,4 +1,5 @@
 CFLAGS = -g -O2 -Wall
+CFLAGS += $(shell getconf LFS_CFLAGS)
 
 .PHONY: all
 all: pkvm

commit 38360f1199be755a728b1f49eb2a04583468a2d2

Don't use getconf(1) to acquire LFS build flags.
The use of getconf(1) doesn't play well with cross-compiling.

Set _FILE_OFFSET_BITS=64 directly, which should be good enough for all
practical purposes.

Fixes: 153aaf3f39131c977370e51ae311f59e0cc39d1f
Reverts: 45168935dd2587656b83f01e3bdcb0d8d54c620e
Reverts: bae29fb0f185fbba940f8a6585d1d647695a64f1

Makefile
@@ -1,8 +1,6 @@
 CFLAGS ?= -g -O2
 CFLAGS += -Wall -Wextra -Wconversion
-CFLAGS += $(shell getconf LFS_CFLAGS)
-LDFLAGS += $(shell getconf LFS_LDFLAGS)
-LDLIBS += $(shell getconf LFS_LIBS)
+CFLAGS += -D_FILE_OFFSET_BITS=64
 
 .PHONY: all
 all: recidivm